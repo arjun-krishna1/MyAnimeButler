22958179539a8f268dbcef7c08c232a77aef05ab
arjunkrishna <arjun.krishna@huawei.com>
2021-07-10 16:10:23 -0400

    add anime recommendations

--- cluster.py
+++ cluster.py
@@ -1,7 +1,7 @@
 import json
 with open("users_key_anime_value1.json") as f:
     data = json.load(f)
-    print(data["5])
+    print(data["5"])
 
     
 
@@ -18,4 +18,18 @@ def find_similar_users(user_id):
     user_prevelence.sort(reverse = True, key = lambda x : x[1])
     print(user_prevelence)
     
-    
+def find_similar_animes(user_id, user_prevelence):
+    # get closest user
+    curr_neighbor = user_prevelence[0][0]
+    result = None
+    # while we haven't found a good anime
+    while not result:
+        # get all the animes of this new user
+        curr_animes = user_table[curr_neighbor]
+        # iterate through all the animes
+        for i in curr_animes:
+            # if I haven't watched this
+            if i not in user_table[i]:
+                return i
+    return 1
+
